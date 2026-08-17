2311475ae93ba590ab1cfb29d6abfdb5007a5256
Alexander Zavgorodniy <azavgo@yahoo.com>
2026-08-17 20:50:08 +1000

    Added chrono to read system date and time

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use uuidgen::{UUID, UUIDError};
-use timestampepoch::*;
+use chrono::prelude::*;
 
 fn main() -> Result<(), UUIDError>{
     let uuid = UUID::new()?.uuid();
     println!("\"_id\": \"{}\",", &uuid); 
     
-    let date: Date = Date::new(17, 8, 2026);
+    let today = Local::now();
+    let day = today.day() as usize; 
+    let month = today.month() as usize; 
+    let year = today.year() as usize; 
+
+    let date: timestampepoch::Date = timestampepoch::Date::new(day, month, year);
     let timestamp = date.timestamp(); 
     match timestamp {
         Some(value) => println!("\"payment_date\": {},", value),
