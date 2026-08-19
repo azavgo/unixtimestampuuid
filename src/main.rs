0e9a27f0d1008a2d84ce12f1b250d18d0f4c0b9c
Alexander Zavgorodniy <azavgo@yahoo.com>
2026-08-19 12:33:33 +1000

    Added copy to clipboard functionality

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
-use uuidgen::{UUID, UUIDError};
+use uuidv4::*;
 use chrono::prelude::*;
+use system_helpers::*;
 
-fn main() -> Result<(), UUIDError>{
-    let uuid = UUID::new()?.uuid();
-    println!("\"_id\": \"{}\",", &uuid); 
+//Custom error
+#[derive(Debug)]
+pub enum UnixTimestampUUIDError { 
+    UUIDError(uuidv4::UUIDError),
+    SystemHelpersError(system_helpers::SystemHelpersError),   
+}
+
+impl From<uuidv4::UUIDError> for UnixTimestampUUIDError {
+    fn from(error: uuidv4::UUIDError) -> Self {
+        UnixTimestampUUIDError::UUIDError(error)
+    }
+}
+
+impl From<system_helpers::SystemHelpersError> for UnixTimestampUUIDError {
+    fn from(error: system_helpers::SystemHelpersError) -> Self {
+        UnixTimestampUUIDError::SystemHelpersError(error)
+    }
+}
+fn main() -> Result<(), UnixTimestampUUIDError> {
+    let uuid = uuid()?;
+    //println!("\"_id\": \"{}\",", &uuid); 
     
     let today = Local::now();
     let day = today.day() as usize; 
@@ -11,10 +30,16 @@ fn main() -> Result<(), UUIDError>{
     let year = today.year() as usize; 
 
     let date: timestampepoch::Date = timestampepoch::Date::new(day, month, year);
-    let timestamp = date.timestamp(); 
-    match timestamp {
-        Some(value) => println!("\"payment_date\": {},", value),
-        None               => println!("Timestamp is not available!"),
+    let timestamp_option = date.timestamp(); 
+    match timestamp_option {
+        Some(value) => {
+                                let timestamp = value.to_string(); 
+                                //println!("\"payment_date\": {},", &timestamp);
+                                let output = format!("  \"_id\": \"{}\",\n  \"payment_date\": {},", &uuid, &timestamp);
+                                println!("{}", &output);
+                                copy_to_clipboard(output)?;
+                              },
+        None               => eprintln!("Timestamp is not available!"),
    }
 
     Ok(())
